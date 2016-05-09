b5d638d0e6f7bd3f61f927b43610b62bc82076ec
Ubuntu <ubuntu@ip-172-31-56-176.ec2.internal>
2016-05-09 17:54:35 +0000

    updated server.js and package list

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 const express = require('express')
 const path = require('path')
 var host = '127.0.0.1'
-var port = 3000
+var port = 2222
 
 var app = express()
 app.use(express.static(path.join(__dirname, '/www')))  // use static files in ROOT/www folder
 
+
+/* app.get('/', function (req, res) {
+   res.send('hi!')
+   }) */
+
 app.get('/breakout', function (req, res) {
-  res.sendFile('www/breakout.html')
+  res.sendFile(path.join(__dirname, 'www/breakout.html'))
 })
 
 //  The 404 Route (ALWAYS Keep this as the last route)
 app.get('*', function (req, res) {
-  res.sendFile('www/404.html')
+  res.sendFile(path.join(__dirname, 'www/404.html'))
 })
 
 app.listen(port, host)
